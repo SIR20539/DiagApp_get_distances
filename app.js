1633d94c73c9cbd16bb85be53fe32a42c1758718
SIR20539 <siretskiy@gmail.com>
2022-08-15 13:27:55 +0200

    beta version

---
+++ app.js
@@ -0,0 +1,73 @@
+import { trams, getUrl } from './src/trams.js';
+const tramsDiv = document.getElementById('trams');
+const input = document.querySelector('input');
+const startBtn = document.getElementById('hashBtn');
+
+// input.value = '70f85cef-2e8e-4657-93be-cafcd19668a8';
+const divDistances = document.getElementById('distances');
+
+const createTramIcon = (tramName) => {
+  const btn = document.createElement('button');
+  btn.className = 'trams';
+  btn.textContent = `${tramName}`;
+  tramsDiv.appendChild(btn);
+};
+
+const showDistance = (distance) => {
+  divDistances.innerHTML = '';
+  distance.map((value) => {
+    // console.log(value);
+    const pDist = document.createElement('p');
+    pDist.innerText = `${value.VehNum} ${Math.round(value.Value)}`;
+    divDistances.appendChild(pDist);
+  });
+};
+
+const sortData = (dataObj) => {
+  dataObj.sort((a, b) => {
+    return a.VehNum - b.VehNum;
+  });
+};
+
+const fetchdata = async (url) => {
+  const responce = await fetch(url);
+  const data = await responce.json();
+  const distance = data.Values.Distance;
+  sortData(distance);
+  showDistance(distance);
+};
+
+const getKey = (button) => {
+  const searchIndex = trams.findIndex((tram) => tram.name === `${button}`);
+  return trams[searchIndex].key;
+};
+
+const handleButtons = () => {
+  const tramsBtns = document.querySelectorAll('.trams');
+  for (let item of tramsBtns) {
+    item.addEventListener('click', () => {
+      const url = getUrl(getKey(item.textContent), input.value);
+      fetchdata(url);
+    });
+  }
+};
+
+const getTrams = (input) => {
+  tramsDiv.innerText = '';
+  if (input.value) {
+    trams.map((val) => {
+      createTramIcon(val.name);
+      handleButtons();
+    });
+  } else {
+    alert('Wprowadź kod dostępu');
+  }
+};
+
+startBtn.addEventListener('click', () => {
+  getTrams(input);
+});
+
+// const hash = 'fbfe0d87-1b92-49c4-aaae-6d54eb14f75c';
+
+// console.log(getUrl(trams[0].key, hashInput));
---
+++ src/trams.js
@@ -0,0 +1,48 @@
+export const trams = [
+  {
+    name: '121NAB',
+    key: 'MTIxTmFC',
+  },
+  {
+    name: '121NBT',
+    key: 'MTIxTmJU',
+  },
+  {
+    name: '122NAB-10',
+    key: 'MTIyTmFCLTEw',
+  },
+  {
+    name: '122NAL',
+    key: 'MTIyTmFM',
+  },
+  {
+    name: '122NAL-10',
+    key: 'MTIyTmFMLTEw',
+  },
+  {
+    name: '122NBT',
+    key: 'MTIyTmJU',
+  },
+  {
+    name: '122NBT-10',
+    key: 'MTIyTmJULTEw',
+  },
+  {
+    name: '122NBT DUO',
+    key: 'MTIyTmJUX0RVTw==',
+  },
+  {
+    name: '128NG-10',
+    key: 'MTI4TkctMTA=',
+  },
+  {
+    name: '2015N',
+    key: 'MjAxNU4=',
+  },
+];
+
+export const getUrl = (
+  key,
+  hash
+) => `https://diagapp.pesa.pl:6324/get_rank/${key}/a00c49b5b0f98ca73b7c705b0f9aa3aebb0c65488de6fe99093cb2f9e3eb9944/${hash}
+`;
